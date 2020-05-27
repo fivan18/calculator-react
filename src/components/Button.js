f270ac16733eb7d7595205fb048282a904a61e0b
Ivan <fivanunam@gmail.com>
2020-05-27 13:22:47 -0500

    add style to each component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ value }) => (
-  <button type="button">{ value }</button>
-);
+const Button = ({ value, color, wide }) => {
+  const style = wide ? {backgroundColor: color, width: '50%'} : {backgroundColor: color};
+  return (
+    <button type="button" style={style}>{ value }</button>
+  );
+}
+
+Button.defaultProps = {
+  color: '#f5913e',
+  wide: false
+}
 
 Button.propTypes = {
   value: PropTypes.string.isRequired,
--- src/components/Display.js
+++ src/components/Display.js
@@ -2,7 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Display = ({ result }) => (
-  <div>{ result }</div>
+  <div className="display d-flex">
+    <div>
+      { result }
+    </div>
+  </div>
 );
 
 Display.propTypes = {

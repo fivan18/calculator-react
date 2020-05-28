50196b2d223c39badd056ef95b736e057db4cd1c
Ivan <fivanunam@gmail.com>
2020-05-28 11:48:46 -0500

    generate number

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,8 +1,18 @@
 import Big from 'big.js'; // eslint-disable-line import/extensions
 import operate from './operate';
+import { number } from 'prop-types';
+
+function validNumber(number){
+  return /^(-)?[1-9]\d*(\.\d+)?$/.test(number);
+}
+function changeSign(number){
+  const bigNumber = Big(number);
+  bigNumber.s *= -1;
+  return bigNumber.toString();
+}
 
 const calculations = {
-  AC: () => ({ total: '', next: '', operation: '' }),
+  AC: () => ({ total: null, next: null, operation: null }),
   '+/-': data => {
     let { total, next } = data;
     const { operation } = data;
@@ -21,6 +31,19 @@ const doOperate = data => {
   calculations[operation] = doOperate;
 });
 
+const doGenerateNumber = (data, digit) => {
+  let { next } = data;
+  const { operation, total } = data;
+  if(next){
+    next += digit;
+  } else {
+    next = digit;
+  }
+  return { next, total, operation };
+};
+['0', '1', '2', '3', '4', '5', '6', '7', '8', '9','.'].forEach(digit => {
+  calculations[digit] = doGenerateNumber;
+});
 const calculate = (data, buttonName) => calculations[buttonName](data);
 
 export default calculate;
